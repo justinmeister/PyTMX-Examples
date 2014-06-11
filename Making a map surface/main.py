df20eaa895b17119738138a3bcf70014f5919419
Justin Armstrong <justinmeister@gmail.com>
2014-06-11 12:58:11 -0700

    Added some comments.

--- Making a map surface/main.py
+++ Making a map surface/main.py
@@ -2,19 +2,30 @@ import sys, os
 import pygame as pg
 import tilerender
 
+"""Initialize pygame, create a clock, create the window
+with a surface to blit the map onto."""
 pg.init()
 fps_clock = pg.time.Clock()
 main_surface = pg.display.set_mode((420, 420))
 main_rect = main_surface.get_rect()
 
+
+"""Load the tmx file from the current directory,
+create the tile_renderer object and load the tmx
+file."""
 tmx_file = os.path.join(os.getcwd(), 'test.tmx')
 tile_renderer = tilerender.Renderer(tmx_file)
 
+
+"""Create the map surface using the make_map()
+method.  Used to blit onto the main_surface."""
 map_surface = tile_renderer.make_map()
 map_rect = map_surface.get_rect()
 
+
+"""Simple game loop that blits the map_surface onto
+the main_surface."""
 while True:
-    main_surface.fill((0,0,0))
     main_surface.blit(map_surface, map_rect)
 
     for event in pg.event.get():
